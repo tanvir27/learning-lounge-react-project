f4103cfb9873fcfa2213d98081017fb658be2493
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-03-31 04:40:47 +0600

    sidecart component show in ui

--- src/components/SideCart/SideCart.js
+++ src/components/SideCart/SideCart.js
@@ -5,7 +5,14 @@ import "./SideCart.css";
 const SideCart = () => {
   return (
     <div className="">
-    <h1>this is sidecart</h1>
+      <div className="sideCart_header mb-3">
+        <p className="fs-5 fw-bold text-primary text-center my-auto py-3">
+          Spent Time on Read: {0} min
+        </p>
+      </div>
+      <div className="sideCart-content p-3">
+        <h4 className="fs-5 fw-bold">Bookmarked Blogs : {0}</h4>
+      </div>
     </div>
   );
 };
