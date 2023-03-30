fb05c410efe47d99ea1a9778a79a3f6aa653122b
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-03-31 04:04:10 +0600

    show img singleBlogs data in ui

--- src/components/SingleBlogs/SingleBlogs.js
+++ src/components/SingleBlogs/SingleBlogs.js
@@ -1,10 +1,25 @@
 import React from "react";
-import './SingleBlogs.css'
+import "./SingleBlogs.css";
 
 const SingleBlogs = ({ blog }) => {
   return (
     <div className="movie-card card mb-2 ">
-      <h1>This is single cart page</h1>
+      <div className="movie-poster ">
+        <img
+          className="img-fluid w-100 rounded"
+          src={blog.cover_image}
+          alt=""
+        />
+      </div>
+
+      <h3 className="">gfd</h3>
+      <h1>{blog.blog_title}</h1>
+      <div className="d-flex justify-content-around">
+        <p className=""></p>
+        <p className=""></p>
+      </div>
+
+      <button className="w-50 text-start">Mark as Read</button>
     </div>
   );
 };
